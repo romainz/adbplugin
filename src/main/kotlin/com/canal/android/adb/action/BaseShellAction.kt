a32dc2aeedd9d32e3786f1ff747a105715ed34a5
Romain Zanon <romain.zanon@canal-plus.com>
2021-03-28 23:45:36 +0200

    add setting to display notification with shell command
    documentation: https://github.com/JetBrains/intellij-sdk-code-samples/tree/main/settings

--- src/main/kotlin/com/canal/android/adb/action/BaseShellAction.kt
+++ src/main/kotlin/com/canal/android/adb/action/BaseShellAction.kt
@@ -2,14 +2,21 @@ package com.canal.android.adb.action
 
 import com.android.ddmlib.IDevice
 import com.android.ddmlib.NullOutputReceiver
+import com.canal.android.adb.setting.AdbPluginSettingsState
+import com.canal.android.adb.util.showNotification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
 
 abstract class BaseShellAction : BaseDeviceAction() {
 
-    override fun IDevice.process() {
-        this.executeShellCommand(
+    override fun process(project: Project, device: IDevice) {
+        device.executeShellCommand(
             getShellCommand(),
             NullOutputReceiver()
         )
+        if (AdbPluginSettingsState.instance.displayAdbNotification) {
+            project.showNotification("adb shell ${getShellCommand()}", NotificationType.INFORMATION)
+        }
     }
 
     abstract fun getShellCommand(): String
