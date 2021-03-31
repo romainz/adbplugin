8027eafb74d39c46ec664ead7ede2a9e569292bb
Romain Zanon <romain.zanon@canal-plus.com>
2021-04-01 00:54:14 +0200

    action - add start application action

--- src/main/kotlin/com/canal/android/adb/action/BaseShellAction.kt
+++ src/main/kotlin/com/canal/android/adb/action/BaseShellAction.kt
@@ -1,8 +1,8 @@
 package com.canal.android.adb.action
 
 import com.android.ddmlib.IDevice
-import com.android.ddmlib.NullOutputReceiver
 import com.canal.android.adb.setting.AdbPluginSettingsState
+import com.canal.android.adb.util.ShellReceiver
 import com.canal.android.adb.util.showNotification
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
@@ -12,7 +12,7 @@ abstract class BaseShellAction : BaseDeviceAction() {
     override fun process(project: Project, device: IDevice) {
         device.executeShellCommand(
             getShellCommand(),
-            NullOutputReceiver()
+            ShellReceiver.getReceiver()
         )
         if (AdbPluginSettingsState.instance.displayAdbNotification) {
             project.showNotification("adb shell ${getShellCommand()}", NotificationType.INFORMATION)
