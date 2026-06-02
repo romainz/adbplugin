87eb0d0798a118d3a82d9e93446d1d256ddb500a
Romain Zanon <romain.zanon@canal-plus.com>
2026-06-02 14:04:31 +0200

    open plugin setting from tool window

--- src/main/kotlin/com/zanon/android/adb/panel/AdbToolsToolWindowFactory.kt
+++ src/main/kotlin/com/zanon/android/adb/panel/AdbToolsToolWindowFactory.kt
@@ -1,10 +1,15 @@
 package com.zanon.android.adb.panel
 
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import com.zanon.android.adb.setting.AdbPluginSettingsConfigurable
 
 class AdbToolsToolWindowFactory : ToolWindowFactory, DumbAware {
 
@@ -13,6 +18,21 @@ class AdbToolsToolWindowFactory : ToolWindowFactory, DumbAware {
         val content = ContentFactory.getInstance()
             .createContent(adbToolsPanel, "", false)
 
-        toolWindow.contentManager.addContent(content)
+        toolWindow.apply {
+            contentManager.addContent(content)
+            setTitleActions(listOf(OpenSettingsAction(project)))
+        }
+    }
+
+    private class OpenSettingsAction(
+        private val project: Project
+    ) : AnAction("Settings", "Open ADB Tools settings", AllIcons.General.Settings) {
+
+        override fun actionPerformed(event: AnActionEvent) {
+            ShowSettingsUtil.getInstance().showSettingsDialog(
+                project,
+                AdbPluginSettingsConfigurable::class.java
+            )
+        }
     }
 }
